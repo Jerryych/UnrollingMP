fc8457cc4f4a63a74f9ffbe920b0efeb0f9a82fb
Jerryych <jerry.yeh0531@gmail.com>
2021-02-16 22:45:58 +0800

    modify train.py: command line arguments passing done

--- train.py
+++ train.py
@@ -2,6 +2,7 @@
 import numpy as np
 import h5py
 from model import MP
+import sys
 
 
 def load_data(dir, fname):
@@ -9,7 +10,7 @@ def load_data(dir, fname):
     f = h5py.File(path, 'r')
     return f['X'][()], f['Y'][()], f['phi'][()], f['m'][()]
 
-def main(dir, fname, model):
+def main(dir, fname, model, mode):
     '''
     Load data and start training.
     '''
@@ -18,7 +19,7 @@ def main(dir, fname, model):
     n, _ = phi.shape
     if model == 'MP':
         mp = MP(N, n, p)
-        mp.fit(Y, m, phi, X)
+        mp.fit(Y, m, phi, X, mode=mode)
     elif model == 'UMP':
         pass
     else:
@@ -26,4 +27,11 @@ def main(dir, fname, model):
 
 
 if __name__ = '__main__':
-    main(dir='dataset', fname='arti_dataset.hdf5', model='MP')
+    if len(sys.argv) == 5:
+        dir = sys.argv[1]
+        fname = sys.argv[2]
+        model = sys.argv[3]
+        m_mode = sys.argv[4]
+        main(dir=dir, fname=fname, model=model, mode=m_mode)
+    else:
+        print('Not enough arguments...')
