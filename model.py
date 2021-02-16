eeae8a12ea69cc8ecc42ec89202a642c05d5c72b
Jerryych <jerry.yeh0531@gmail.com>
2021-02-16 21:56:58 +0800

    implement model.py: class MP

--- model.py
+++ model.py
@@ -1,5 +1,6 @@
 import numpy as np
 from numpy import linalg as LA
+import math
 
 
 class MP:
@@ -9,44 +10,115 @@ class MP:
         N: number of data instance
         n: dimension of signal
         p: number of atom
+        step: step size for update
+        rho: lagrange multiplier for constraint m
+        lam: hyper-param for proximal gradient
+        threshold: shrinkage operator
         '''
         self.phi = np.random.randn(n, p)
         self.x_p = np.random.randn(p, N)
         self.N = N
         self.n = n
         self.p = p
+        self.step = 0.1
+        self.rho = -1.0
+        self.lam = 1.0
+        self.threshold = math.sqrt(2 * (-1 * self.rho) / self.lam)
 
-    def fit(self, Y, m, phi_real, X):
+    def fit(self, Y, m, phi_real, X, mode='m'):
         '''
-        fit (phi, X)
+        Fit (phi, X)
 
         X: sparse representation
         Y: signal
         m: number of atom in sparse representation
+        mode: 'm' for 1 fit_all_instances() with constraint m, '1' for m fit_all_instances() with increasing constraint
         '''
         self.y = Y
 
-        for i in range(m):
-            for j in range(self.N):
-                self.__update_curr_x(i, j)
-                self.__update_past_x(i, j - 1)
-                self.__update_phi(i, j)
+        if mode == 'm':
+            self.fit_all_instances(m)
             p_d, x_d = self.eval(phi_real, X)
             print(f'Phi diff: {p_d}, X diff: {x_d}')
+        elif mode == '1':
+            for i in range(m):
+                self.fit_all_instances(i)
+                p_d, x_d = self.eval(phi_real, X)
+                print(f'Phi diff: {p_d}, X diff: {x_d}')
+                self.y = self.y - self.phi @ self.x_p
+
+    def fit_all_instances(self, m):
+        '''
+        Fit (phi, X) given m
+        '''
+        for j in range(self.N):
+            self.__update_curr_x(m, j)
+            self.__update_past_x(m, j - 1)
+            self.__update_phi(j)
 
     def __update_curr_x(self, m, j):
+        '''
+        Proximal gradient update of j-th column of x
+        '''
+        self.x_p[:, j] = self.x_p[:, j] - self.__grad_x(j)
+        self.x_p[:, j] = self.__prox_grad_x(m, j)
 
     def __update_past_x(self, m, j):
+        '''
+        Proximal gradient update of x from 0 to j - 1 (included)
+        Column by column update
+        '''
+        j = j + 1
+        for idx in range(j):
+            self.x_p[:, idx] = self.x_p[:, idx] - self.__grad_x(idx)
+            self.x_p[:, idx] = self.__prox_grad_x(m, j)
 
-    def __udpate_phi(self, m, j):
+    def __udpate_phi(self, j):
+        '''
+        Update phi with {x[0] ~ x[j]}
+        Matrix update
+        '''
+        self.phi = self.phi - self.__grad_phi(j)
 
-    def eval(self, phi_real, X):
+    def __grad_x(self, j):
+        '''
+        In vector form
+        d_x = phi' * (y - phi * x)
+        '''
+        return -1 * self.step * self.phi.T @ (self.y[:, j] - self.phi @ self.x_p[:, j])
+
+    def __prox_grad_x(self, m, j):
+        '''
+        In vector form
+        Hard thresholding
+        '''
+        return np.where(np.absolute(x) > self.threshold, x, 0)
+
+    def __grad_phi(self, j):
+        '''
+        In matrix form
+        d_phi = 4 * phi * (phi' * phi - I) - 2 * (y - phi * x) * x'
+        '''
+        j = j + 1
+        return 4 * self.phi @ (self.phi.T @ self.phi - np.eye(self.p)) - 2 * (self.y[:, : j] - self.phi @ self.x_p[:, : j]) @ self.x_p[:, : j].T
+
+    def get_X(self):
+        return self.x_p
+
+    def get_phi(self):
+        return self.phi
+
+    def eval(self, phi_real, X, Y=None):
         '''
         measure difference between estimated (phi, X) and real (phi, X) with 2-norm
         '''
         phi_diff = LA.norm(phi_real - self.phi)
         X_diff = LA.norm(X - self.x_p)
-        return phi_diff, X_diff
+        if Y:
+            Y_diff = LA.norm(Y - self.phi @ self.x_p)
+            return phi_diff, X_diff, Y_diff
+        else:
+            return phi_diff, X_diff
 
 
 class UMP:
